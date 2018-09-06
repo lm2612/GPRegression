6ce3383ffa5072dc0db8b40d05c169de02d1cc11
lm2612 <lm2612@ic.ac.uk>
2018-09-06 23:39:43 +0100

    better assert statement and sklearn imported

--- SplitTrainingAndTesting.py
+++ SplitTrainingAndTesting.py
@@ -1,6 +1,14 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
 def split_set(X,y,names,TestNames):
     """ Manually select test data to split for training and testing """
-    assert (all([TestName in names for TestName in TestNames])),'All scenarios listed in TestNames must be in names. Check for typos. TestNames= {0}. names={1}'.format(TestNames,names)
+    assert (type(TestNames) is list),'TestNames should be list'
+    # Check all listed scenarios are in names
+    check = ([TestName in names for TestName in TestNames])
+    assert (all(check)),'All scenarios listed in TestNames must be in names. Check for typo in following: {}'.format(list(np.array(TestNames)[[not i for i in check]])) 
+
     idx_test = [names.index(TestName) for TestName in TestNames]
     idx_train = [idx for idx in range(len(names)) if idx!=idx_test]
     X_train,X_test = X[idx_train],X[idx_test]
@@ -14,8 +22,7 @@ def split_set_random(X,y,names,test_number,seed):
     """ Splits set into a random test set and training set based on seed,
     with number of test sets being test_number and training sets being N-test_number """
     (N,p) = X.shape
-    assert (test_number<N),"Select test_number to be less than total number of samples
-    in set. test_number={0}, number of samples={1}".format(test_number,N)
+    assert (test_number<N),"Select test_number to be less than total number of samples in set. test_number={0}, number of samples={1}".format(test_number,N)
     idxs = np.arange(N)
     (X_train,X_test,y_train,y_test,idx_train,idx_test) = train_test_split(X,y,idxs,test_size=np.float(test_number)/np.float(N),random_state=seed)
     names_train = [names[idx] for idx in idx_train]
